666e0c0e5bd5e35cec1adae38dae4b0eeb78365b
Christian Feichtinger <cf@ptscientists.com>
2020-03-19 11:24:41 +0100

    - All files renamed consistently to "assemblyLover"

--- AssemblyLover-Build.bat
+++ AssemblyLover-Build.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-"avrasm2.exe" -S "labels.tmp" -fI -W+ie -C V2E -o "ass_seminar.hex" -d "ass_seminar.obj" -e "ass_seminar.eep" -m "ass_seminar.map" -l "ass_seminar.lst" "ass_seminar.asm"
+"avrasm2.exe" -S "assemblyLover-Labes.tmp" -fI -W+ie -C V2E -o "assemblyLover.hex" -d "assemblyLover.obj" -e "assemblyLover.eep" -m "assemblyLover.map" -l "assemblyLover.lst" "assemblyLover.asm"
